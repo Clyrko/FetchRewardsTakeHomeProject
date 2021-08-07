d19acabee3a600f877a7f32bd8ca84bc6711146a
Jay A <justynadusei@gmail.com>
2021-08-07 15:12:11 +0000

    Initial Commit

---
+++ Take Home Project - FetchRewards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Take Home Project - FetchRewards
+//
+//  Created by Jay A. on 07/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
